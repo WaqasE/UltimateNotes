0b6f52b9f349c7b1b9a848b8e7b7e86d7e5c86de
Waqas Ehmed <waqasehmed1@gmail.com>
2021-03-28 09:14:09 +0500

    sixth commit

--- routes/getNotes.js
+++ routes/getNotes.js
@@ -2,16 +2,19 @@ const router = require('express').Router();
 const { Note } = require('../model/notes');
 const { User } = require('../model/users');
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     if (req.body._id) {
-        let user = await User.findById({ _id: req.body._id })
+        let user = await User.findById(req.body._id)
         if (!user) { res.send('bad request').status(400); }
-        const note = new Note({ title: req.body.title, body: req.body.body, userId: user._id, date: req.body.date, time: req.body.time, edited: false })
-        note.save(note);
-        res.send('note saved succefully').status(200);
+        else {
+            const list = await Note.find({ userId: req.body._id });
+            res.send(list).sendStatus(200);
+        }
 
     }
-    res.send('bad request').status(400);
+    else {
+        res.send('bad request').status(400);
+    }
 })
 
 module.exports = router;
