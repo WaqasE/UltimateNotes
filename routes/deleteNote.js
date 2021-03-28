103207d3d2b42fb718b230a38a94e6f94f624423
Waqas Ehmed <waqasehmed1@gmail.com>
2021-03-28 10:02:12 +0500

    seventh commit

--- routes/deleteNote.js
+++ routes/deleteNote.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
 const { Note } = require('../model/notes');
+const { User } = require('../model/users');
 
-router.post('/',({req, res})=>{
-    res.send('ok').status(200);
+router.post('/', async (req, res) => {
+    if (req.body._id) {
+        let note = await Note.findById(req.body._id)
+        note.delete().then((result, err) => {
+            if (err) {
+                console.log(err)
+                res.sendStatus(400);
+            }
+            else {
+                res.send('note deleted sucessfully.').status(200);
+
+            }
+        })
+    }
+    else {
+        res.send('bad request').status(400);
+    }
 })
 
 module.exports = router;
--- routes/editNote.js
+++ routes/editNote.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
 const { Note } = require('../model/notes');
+const { User } = require('../model/users');
 
-router.post('/',({req, res})=>{
-    res.send('ok').status(200);
+router.post('/', async (req, res) => {
+    if (req.body._id) {
+        let note = await Note.findById(req.body._id)
+        if (!note) { res.send('bad request').status(400); }
+        note.title = req.body.title;
+        note.body = req.body.body;
+        note.userId = req.body.userId;
+        note.date = req.body.date;
+        note.time = req.body.time;
+        note.edited = req.body.edited;
+        await note.save(note);
+        res.send('note saved edited').status(200);
+
+    }
+    else {
+        res.send('bad request').status(400);
+    }
 })
 
 module.exports = router;
